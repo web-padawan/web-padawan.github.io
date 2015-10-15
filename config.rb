ff65d94673ef50d49e8f507f3183fa7815b09330
web-padawan <skamandr@yandex.ru>
2015-10-15 08:39:53 +0300

    [mod] using compass without plugin

---
+++ config.rb
@@ -0,0 +1,31 @@
+# Default to development if environment is not set.
+saved = environment
+if (environment.nil?)
+  environment = :development
+else
+  environment = saved
+end
+
+# Location of the theme's resources.
+css_dir = "assets/css"
+sass_dir = "sass"
+images_dir = "images"
+generated_images_dir = images_dir + "/generated"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+# Require any additional compass plugins installed on your system.
+require 'toolkit'
+require 'breakpoint'
+require 'susy'
+require 'sass-globbing'
+add_import_path "bower_components/normalize-scss"
+
+# Select output style (:expanded, :nested, :compact, :compressed).
+output_style = (environment == :production) ? :compressed : :expanded
+
+# Enable relative paths to assets via compass helper functions.
+relative_assets = true
+
+# Disable output line comments in CSS.
+line_comments = false
