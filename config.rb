4be8c6839355525d37a5f0b939d3972eb8d005d0
web-padawan <skamandr@yandex.ru>
2015-10-16 09:34:48 +0300

    [new] gulp, some config

--- config.rb
+++ config.rb
@@ -7,19 +7,20 @@ else
 end
 
 # Location of the theme's resources.
-css_dir = "assets/css"
-sass_dir = "sass"
-images_dir = "assets/images"
-generated_images_dir = images_dir + "/generated"
-javascripts_dir = "assets/js"
-fonts_dir = "assets/fonts"
+http_path = '/'
+css_dir = 'assets/css'
+sass_dir = 'sass'
+images_dir = 'assets/images'
+generated_images_dir = images_dir + '/generated'
+javascripts_dir = 'assets/js'
+fonts_dir = 'assets/fonts'
 
 # Require any additional compass plugins installed on your system.
 require 'toolkit'
 require 'breakpoint'
 require 'susy'
 require 'sass-globbing'
-add_import_path "bower_components/normalize-scss"
+add_import_path 'bower_components/normalize-scss'
 
 # Select output style (:expanded, :nested, :compact, :compressed).
 output_style = (environment == :production) ? :compressed : :expanded
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp          = require('gulp'),
+    compass       = require('gulp-compass'),
+    csso          = require('gulp-csso'),
+    rename        = require('gulp-rename'),
+    postcss       = require('gulp-postcss'),
+    autoprefixer  = require('autoprefixer'),
+    path          = require('path');
+
+gulp.task('compile', function() {
+  gulp.src('./sass/**/*.scss')
+    .pipe(compass({
+      config_file: './config.rb',
+      css: 'assets/css',
+      bundle_exec: true
+    }))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(csso())
+    .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('default', function() {
+  gulp.watch('sass/**/**/*.scss', ['compile']);
+});
