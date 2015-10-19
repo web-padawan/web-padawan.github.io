64cd77655bc054ed984c3f9c24579c6c39b6d66a
web-padawan <skamandr@yandex.ru>
2015-10-19 09:26:45 +0300

    [new] browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,29 @@ var gulp          = require('gulp'),
     rename        = require('gulp-rename'),
     postcss       = require('gulp-postcss'),
     autoprefixer  = require('autoprefixer'),
+    browserSync   = require('browser-sync'),
+    cp            = require('child_process'),
     path          = require('path');
 
-gulp.task('compile', function() {
-  gulp.src('./sass/**/*.scss')
+gulp.task('jekyll-build', function (done) {
+  return cp.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+  browserSync.reload();
+});
+
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+  browserSync.init({
+    server: {
+      baseDir: '_site'
+    }
+  });
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
     .pipe(compass({
       config_file: './config.rb',
       css: 'assets/css',
@@ -18,9 +37,14 @@ gulp.task('compile', function() {
     .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
     .pipe(rename({ suffix: '.min' }))
     .pipe(csso())
-    .pipe(gulp.dest('assets/css'));
+    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('_site/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('default', function() {
-  gulp.watch('sass/**/**/*.scss', ['compile']);
+gulp.task('watch', function () {
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
+
+gulp.task('default', ['browser-sync', 'watch']);
