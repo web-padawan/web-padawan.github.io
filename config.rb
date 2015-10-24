7423a55578f7613e74b47d5f54066cf8a78b4690
web-padawan <skamandr@yandex.ru>
2015-10-24 09:36:57 +0300

    [new] slick.js carousel setup for portfolio

--- config.rb
+++ config.rb
@@ -21,6 +21,7 @@ require 'breakpoint'
 require 'susy'
 require 'sass-globbing'
 add_import_path 'bower_components/normalize-scss'
+add_import_path 'bower_components/slick-carousel/slick'
 
 # Select output style (:expanded, :nested, :compact, :compressed).
 output_style = (environment == :production) ? :compressed : :expanded
