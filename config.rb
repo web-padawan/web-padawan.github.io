f82e1fb44e7bd974ee6025e9a7682905a3dde3d8
web-padawan <skamandr@yandex.ru>
2015-12-27 17:04:03 +0200

    [fix] better way to get static sprites

--- config.rb
+++ config.rb
@@ -32,33 +32,36 @@ relative_assets = true
 # Disable output line comments in CSS.
 line_comments = false
 
-# ===================
 # Uncomment this part to make sprite name static, icons-123asda.png -> icons.png
-# useful for diffs in screen.css
 # ===================
-# Make a copy of sprites with a name that has no uniqueness of the hash.
-on_sprite_saved do |filename|
-  if File.exists?(filename)
-    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+module Compass::SassExtensions::Functions::Sprites
+  def sprite_url(map)
+    verify_map(map, "sprite-url")
+    map.generate
+    generated_image_url(Sass::Script::String.new(map.name_and_hash))
   end
 end
-# Replace in stylesheets generated references to sprites
-# by their counterparts without the hash uniqueness.
-on_stylesheet_saved do |filename|
-  if File.exists?(filename)
-    css = File.read filename
-    File.open(filename, 'w+') do |f|
-      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
+
+module Compass::SassExtensions::Sprites::SpriteMethods
+  def name_and_hash
+    "#{path}.png"
+  end
+
+  def cleanup_old_sprites
+    Dir[File.join(::Compass.configuration.generated_images_path, "#{path}.png")].each do |file|
+      log :remove, file
+      FileUtils.rm file
+      ::Compass.configuration.run_sprite_removed(file)
     end
   end
 end
 
-# # this one for switching between production and dev versions of the website
-# module Sass::Script::Functions
-#   def image_path(string)
-#     assert_type string, :String
-#     Sass::Script::String.new("../img/#{string.value}")
-#   end
-#   alias_method :"image-path",:image_path
-#   declare :"image-path", :args => [:string]
-# end
+module Compass
+  class << SpriteImporter
+    def find_all_sprite_map_files(path)
+      glob = "*{#{self::VALID_EXTENSIONS.join(",")}}"
+      Dir.glob(File.join(path, "**", glob))
+    end
+  end
+end
+# ===================
