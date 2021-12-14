4e5ccfca19fe9a23a9cd97c0b38c4a997ee0804c
web-padawan <iamkulykov@gmail.com>
2021-12-14 17:01:53 +0200

    Sort posts with the same date

--- eleventy/nunjucks-filters/group-by-year.js
+++ eleventy/nunjucks-filters/group-by-year.js
@@ -1,3 +1,5 @@
+const regExp = /(\d{4})-(\d{2})/;
+
 function groupByYear(pages) {
   const pagesByYear = new Map();
 
@@ -13,7 +15,24 @@ function groupByYear(pages) {
   );
 
   sortedYears.forEach(([, pages]) => {
-    pages.sort((a, b) => b.date - a.date);
+    pages.sort((a, b) => {
+      const d1 = new Date(a.date);
+      const d2 = new Date(b.date);
+
+      // Two posts with the same date
+      if (d1.getTime() === d2.getTime()) {
+        // Get post number from URL
+        const m1 = a.url.match(regExp)
+        const m2 = b.url.match(regExp);
+
+        const n1 = m1 && parseInt(m1[2]);
+        const n2 = m2 && parseInt(m2[2]);
+
+        return n2 - n1;
+      }
+
+      return b.date - a.date;
+    });
   });
 
   return sortedYears;
